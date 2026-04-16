793755ce4440747682381a71f0f10743037c888f
Minttu <ag4021@student.jamk.fi>
2026-04-16 17:21:17 +0300

    task 2 completed

--- tht/tht_2/mongo/mongo/deletestudent.js
+++ tht/tht_2/mongo/mongo/deletestudent.js
@@ -1,2 +1,15 @@
 import './dbconnect.js';
 import mongoose from 'mongoose';
+import Student from './models/Student.js';
+
+async function deleteStudent(student_code) {
+  await Student.deleteOne({ studentcode: student_code })
+    .then(console.log(`Student deleted.`))
+    .catch((err) => {
+      throw err;
+    });
+
+  mongoose.disconnect();
+}
+
+deleteStudent('m1234');
--- tht/tht_2/mongo/mongo/updategrade.js
+++ tht/tht_2/mongo/mongo/updategrade.js
@@ -2,4 +2,22 @@ import './dbconnect.js';
 import mongoose from 'mongoose';
 import Student from './models/Student.js';
 
-async function updateGrade(student_code, course_code, newGrade) {}
+async function updateGrade(student_code, course_code, newGrade) {
+  const result = await Student.updateOne(
+    {
+      studentcode: student_code,
+      'grades.coursecode': course_code,
+    },
+    { $set: { 'grades.$.grade': newGrade } },
+  ).catch((err) => {
+    throw err;
+  });
+
+  console.log(
+    `Student with code ${student_code} grade has been updated for course ${course_code}. New grade ${newGrade}`,
+  );
+  console.log(result);
+  mongoose.disconnect();
+}
+
+updateGrade('m1234', 'HTS10600', 2);
